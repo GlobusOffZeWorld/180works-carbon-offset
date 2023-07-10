f68661c18c483fec403bfba19dd15137ec2ba93f
GoggleEyed <GoggleEyed@protonmail.com>
2023-07-10 18:29:39 +0300

    Add summary table

--- server/ddl.sql
+++ server/ddl.sql
@@ -3,4 +3,11 @@ create table raw_temperature (
 	id_stove integer not null,
 	"date" timestamp not null,
 	temperature numeric(4, 1) not null
-)
+);
+
+create table temperature (
+	"date" timestamp not null,
+	stoves_num integer not null, 
+	work_num integer not null, 
+	work_time numeric(8, 2) not null
+);
