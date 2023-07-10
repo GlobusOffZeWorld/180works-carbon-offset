30edb0da56e1bc944f32524e896a4b8c677cf5ca
togramort <togramort@gmail.com>
2023-07-10 20:55:18 +0300

    some touch ups

--- server/ddl.sql
+++ server/ddl.sql
@@ -3,11 +3,11 @@ create table raw_temperature (
 	id_stove integer not null,
 	"date" timestamp not null,
 	temperature numeric(4, 1) not null
-);
+)
 
 create table temperature (
 	"date" date primary key,
-	stoves_num integer not null, 
-	work_num integer not null, 
-	work_time numeric(8, 2) not null
-);
+	stoves_num bigint,
+	work_num bigint,
+	work_time numeric(8,2)
+)
