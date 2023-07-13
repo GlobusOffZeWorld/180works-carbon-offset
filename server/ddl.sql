33c1eb11644bd51a3288d9d01fae13b2502745cf
togramort <togramort@gmail.com>
2023-07-13 20:49:52 +0300

    update db

--- server/ddl.sql
+++ server/ddl.sql
@@ -1,19 +1,26 @@
-create table locations (
-	id_stove integer primary key,
-	country varchar not null,
-	province varchar not null
+create table district (
+	id_district integer primary key,
+	district varchar not null,
+	country varchar not null
 );
 
 create table raw_temperature (
-	id_record serial primary key,
-	id_stove integer not null references locations(id_stove),
+	id_stove integer not null
 	"date" timestamp not null,
-	temperature numeric(4, 1) not null
+	temperature numeric(4, 1) not null,
+	primary key (id_stove, "date")
 );
 
 create table temperature (
-	"date" date primary key,
+	"date" date not null,
 	stoves_num integer not null,
 	work_num integer not null,
-	work_time numeric(8,2) not null
+	work_time numeric(8,2) not null,
+	id_district integer references district(id_district),
+	primary key (id_district, "date")
+);
+
+create table locations (
+	id_stove integer primary key,
+	id_district integer references district(id_district)
 );
