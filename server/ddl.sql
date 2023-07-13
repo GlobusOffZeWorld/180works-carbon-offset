040e1eec195b2f54085da393445601c148c8dcbd
togramort <togramort@gmail.com>
2023-07-13 21:02:32 +0300

    touch ups

--- server/ddl.sql
+++ server/ddl.sql
@@ -1,11 +1,11 @@
-create table district (
+create table districts (
 	id_district integer primary key,
 	district varchar not null,
 	country varchar not null
 );
 
 create table raw_temperature (
-	id_stove integer not null
+	id_stove integer not null,
 	"date" timestamp not null,
 	temperature numeric(4, 1) not null,
 	primary key (id_stove, "date")
@@ -16,11 +16,11 @@ create table temperature (
 	stoves_num integer not null,
 	work_num integer not null,
 	work_time numeric(8,2) not null,
-	id_district integer references district(id_district),
+	id_district integer references districts(id_district),
 	primary key (id_district, "date")
 );
 
 create table locations (
 	id_stove integer primary key,
-	id_district integer references district(id_district)
+	id_district integer references districts(id_district)
 );
